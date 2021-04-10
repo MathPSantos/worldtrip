3e6b0400d07589bcf4050f3dfeed95cef5902f63
MathPSantos <matheus.psantos2016@gmail.com>
2021-04-10 11:22:40 -0300

    feat: add Header to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,7 +10,26 @@ export default function Home() {
         <title>Home</title>
       </Head>
 
-      <h1>Hello World</h1>
+      <Header />
+      <Box as="main" bg="blue.700" w="100%">
+        <Flex
+          maxW={1120}
+          mx="auto"
+          py="16"
+          px="4"
+          align="center"
+          justify="space-between"
+        >
+          <Flex flexDir="column" align="start">
+            <Text color="gray.50" fontWeight="bold">
+              5 Continentes, infinitas possibilidades.
+            </Text>
+            <Text>
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.{" "}
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
     </>
   );
 }
